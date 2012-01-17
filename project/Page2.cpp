cb55ceb414cd122e0f0ed643536cd4ec06ccbb46
Artemis Llamosi <therealarty@gmail.com>
2012-01-17 17:52:19 +0100

    Adding some real files

---
+++ project/Page2.cpp
@@ -0,0 +1,11 @@
+/*
+ *  Page2.cpp
+ *  project
+ *
+ *  Created by Artémis Llamosi on 17/01/12.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "Page2.h"
+
---
+++ project/Page2.h
@@ -0,0 +1,9 @@
+/*
+ *  Page2.h
+ *  project
+ *
+ *  Created by Artémis Llamosi on 17/01/12.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
